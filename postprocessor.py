a301c4b09bff2015e05168065b5af311a838a5d5
Thomas Nicollet <thomas.nicollet@epitech.eu>
2020-02-20 19:23:17 +0100

    Changed to postprocessor

--- postprocessor.py
+++ postprocessor.py
@@ -1,11 +1,6 @@
 import sys
 import json
 
-"""
-
-"""
-
-
 def preprocess_data(data):
     result = [str(len(data["libraries"]))]
     for library in data["libraries"]:
@@ -19,19 +14,6 @@ def preprocess_data(data):
 if __name__ == "__main__":
     with open(sys.argv[1], "r") as f:
         data = json.loads(f.read())
-    #data = main(data)
-    data = {
-        "libraries": [
-            {
-                "id": 0,
-                "books": [0, 1, 2, 3, 4]
-            },
-            {
-                "id": 1,
-                "books": [5]
-            }
-        ]
-    }
     data = preprocess_data(data)
     with open(sys.argv[2], "w") as f:
         f.write(data)
